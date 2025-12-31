ab52165117310521879e8b8aabf91b80f876f9c1
Emma <contact@emma.pizza>
2025-12-31 11:47:32 +0100

    rearrange a thing

--- module/script.nix
+++ module/script.nix
@@ -15,8 +15,8 @@ in runCommand "snaphot-script" {
     --subst-var-by userName    "${user.name}"               \
     --subst-var-by keepWeeks   "${toString cfg.keep.weeks}" \
     --subst-var-by keepDays    "${toString cfg.keep.days}"  \
-    --subst-var-by debug       "${if cfg.debug then "true" else "false"}" \
     --subst-var-by trashDirs   "${escapeShellArgs cfg.trashDirs}" \
+    --subst-var-by debug       "${if cfg.debug then "true" else "false"}" \
     --subst-var-by extraPATH   "${makeBinPath [ util-linux findutils gawk ]}"
   chmod 755 $out/bin/create-snapshot
   ${getExe shellcheck} --exclude=SC2164 $out/bin/create-snapshot
