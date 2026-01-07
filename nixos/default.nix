0cb5f74e0f8ed7d48c719a9e2b00077afed61f88
Emma <contact@emma.pizza>
2026-01-08 00:06:38 +0100

    storage space fix: clean old directories and always clean trash on an hourly basis
    forgive me I made this change at midnight and I am caffeinated and
    sleepy
    
    BUT!!!
    
    I added two new services, one will remove the user snapshots of no
    longer existing users and the other one will make sure that the .trash
    subdirectory is emptied. If it SOMEHOW still doesn't empty then I
    will file a formal complaint to the universe.
    
    I tested this in the vm... iunno, should work on your system :)

--- nixos/default.nix
+++ nixos/default.nix
@@ -1,8 +1,8 @@
 { config, pkgs, lib, ... }:
 
 let
-  inherit (lib) mkIf mkMerge pipe getExe flatten;
-  inherit (pkgs) callPackage;
+  inherit (lib) mkIf mkMerge pipe getExe flatten optionals;
+  inherit (pkgs) callPackage getent findutils;
   inherit (builtins) map;
   
   cfg = config.services.snapshot-thingie;
@@ -14,6 +14,20 @@ in
   ];
   
   config.systemd = {
+    tmpfiles.rules =
+    (pipe cfg.users [
+      (map (elem: config.users.users.${elem}))
+      (map (
+        user: [
+          "d ${cfg.prefix}/${user.name} 0755 ${user.name} ${user.group} - -"
+        ]
+      ))
+      flatten
+    ])
+    ++ [
+      "d  ${cfg.prefix}        0755 root root - -"
+      "d! ${cfg.prefix}/.trash 1777 root root 0 -"
+    ];
     services = pipe cfg.users [ (map (x: config.users.users.${x})) (map ({ name, home, ... }@user: {
       "snapshot-home-${name}" = {
         description = "Snapshot ${home}";
@@ -21,9 +35,12 @@ in
         wants = mkIf cfg.runOnActivation [
           "multi-user.target"
         ];
-        after = mkIf cfg.runOnActivation [
+        after = [
+          "snapshot-clean-removed-users.service"
+          "snapshot-clean-trash.service"
+        ] ++ (optionals cfg.runOnActivation [
           "multi-user.target"
-        ];
+        ]);
         wantedBy = mkIf cfg.runOnActivation [
           "multi-user.target"
         ];
@@ -33,7 +50,31 @@ in
         startAt = mkIf (cfg.onCalendar != null) cfg.onCalendar;
         serviceConfig.IODeviceWeight = [ "${home} 10" "${cfg.prefix} 10" ];
       };
-    })) mkMerge (mkIf cfg.enable) ];
+    })) (services: services ++ [
+      {
+        "snapshot-clean-trash" = {
+          startAt = "hourly";
+          path = [ findutils ];
+          serviceConfig.IODeviceWeight="${cfg.prefix}/.trash 10";
+          script = ''
+            find "${cfg.prefix}/.trash" -mindepth 1 -delete
+          '';
+        };
+        "snapshot-clean-removed-users" = {
+          startAt = "hourly";
+          path = [ getent findutils ];
+          serviceConfig.IODeviceWeight="${cfg.prefix}/.trash 10";
+          script = ''
+            cd "${cfg.prefix}"
+            for i in *; do
+              if ! getent passwd "$i"; then
+                find "$i" -delete
+              fi
+            done
+          '';
+        };
+      }
+    ]) mkMerge (mkIf cfg.enable) ];
 
     timers = pipe cfg.users [ (map (x: config.users.users.${x})) (map ({ name, ... }: {
       "snapshot-home-${name}" = mkIf (cfg.onCalendar != null) {
@@ -52,19 +93,4 @@ in
       };
     })) mkMerge (mkIf cfg.enable) ];
   };
-
-  config.systemd.tmpfiles.rules =
-    (pipe cfg.users [
-      (map (elem: config.users.users.${elem}))
-      (map (
-        user: [
-          "d ${cfg.prefix}/${user.name} 0755 ${user.name} ${user.group} - -"
-        ]
-      ))
-      flatten
-    ])
-    ++ [
-      "d  ${cfg.prefix}        0755 root root - -"
-      "d! ${cfg.prefix}/.trash 1777 root root 0 -"
-    ];
 }
